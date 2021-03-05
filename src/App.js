d0dc954a46134af5a0ae4963e8fb272f3e9d57b3
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-03-05 14:01:58 +0600

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "@material-ui/core/Button";
 import "./App.css";
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
     <>
-      <h1>THis is newspaper.</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </>
   );
 }
